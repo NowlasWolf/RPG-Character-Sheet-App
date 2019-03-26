58f2d2f38cd8e46ab784e2cbc2fb52ad8d1693e3
theonepiecewondo <mdbaerlocher@lcmail.lcsc.edu>
2019-03-26 14:01:27 -0700

    new home smile :)

--- rpg-char-sheet/src/app/app-routing.module.ts
+++ rpg-char-sheet/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'character',
     pathMatch: 'full'
   },
   { path: 'character', loadChildren: './character/character.module#CharacterPageModule' },
