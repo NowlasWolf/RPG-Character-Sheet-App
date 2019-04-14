2c47cca6f90d786865d69909298e241028342f56
Alexander Romero <nowlaswolf@gmail.com>
2019-04-13 21:12:58 -0700

    Database addition to components
    App crashes though with it...

--- rpg-char-sheet/src/app/app.component.ts
+++ rpg-char-sheet/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { DatabaseProvider } from './database';
 
 @Component({
   selector: 'app-root',
@@ -25,12 +26,14 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private database: DatabaseProvider
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
+    this.database.createTables();
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
