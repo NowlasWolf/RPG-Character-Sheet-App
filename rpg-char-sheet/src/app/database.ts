d39186e7ac230936585805b49e64417b3236f0ff
mrvaccaro <46974362+mrvaccaro@users.noreply.github.com>
2019-04-29 16:12:26 -0700

    Update database.ts

--- rpg-char-sheet/src/app/database.ts
+++ rpg-char-sheet/src/app/database.ts
@@ -28,12 +28,12 @@ export class DatabaseProvider
         this.db.executeSql('CREATE TABLE IF NOT EXISTS CHARACTER(ID_NUM int(10) PRIMARY KEY, C_NAME character(50), C_TYPE character(10)', []).catch(e => console.log(e));
         this.db.executeSql('CREATE TABLE IF NOT EXISTS DETAILS(ID_NUM int(10) PRIMARY KEY, AGE int(6), HEIGHT character(10), C_WEIGHT character(10), HAIR character(10), EYES character(15), GENDER character(10), RACE character(20), CLASS character(20), LEVEL int(2), EXPERIENCE int(10))',[]).catch(e => console.log(e));
         this.db.executeSql('CREATE TABLE IF NOT EXISTS STATS(ID_NUM int(10) PRIMARY KEY, STRENGTH int(2), DEXTERITY int(2), CONSTITUTION int(2), INTELLEGENCE int(2), WISDOM int(2), CHARISMA int(2), MAX_HIT_POINTS int(4), HIT_POINTS int(4), BASE_ATTACK_BONUS int(2) INSPIRATION int(1))',[]).catch(e => console.log(e));
-        this.db.executeSql('CREATE TABLE IF NOT EXISTS SKILLS(ID_NUM int(10) PRIMARY KEY, SK_NAME character(10), RANKS int(2), PROFICIENCY int(1))',[]).catch(e => console.log(e));
-        this.db.executeSql('CREATE TABLE IF NOT EXISTS GEAR(ID_NUM int(10) PRIMARY KEY, G_NAME character(32), G_WEIGHT int(4), AMOUNT int(4))', []).catch(e => console.log(e));
-        this.db.executeSql('CREATE TABLE IF NOT EXISTS WEAPONS(ID_NUM int(10) PRIMARY KEY, W_NAME character(32), DAMAGE character(6), TYPE character(12), RANGE int(4), CRITICAL character(6), W_WEIGHT int(4), W_AMOUNT int(4))', []).catch(e => console.log(e));
-        this.db.executeSql('CREATE TABLE IF NOT EXISTS ARMOR(ID_NUM int(10) PRIMARY KEY, AR_NAME character(32), AC_BONUS int(2), MAX_DEX int(2), CHECK int(2), SPELL_FAIL int(2), AR_WEIGHT int(4), AR_AMOUNT int(4))', []).catch(e => console.log(e));
-        this.db.executeSql('CREATE TABLE IF NOT EXISTS ABILITIES(ID_NUM int(10) PRIMARY KEY, AB_NAME character(32), AB_TYPE int(1), AB_LEVEL int(2))', []).catch(e => console.log(e));
-        this.db.executeSql('CREATE TABLE IF NOT EXISTS SPELLS(ID_NUM int(10) PRIMARY KEY, SP_NAME character(32), SP_LEVEL int(1))', []).catch(e => console.log(e));
+        this.db.executeSql('CREATE TABLE IF NOT EXISTS SKILLS(ID_NUM int(10) PRIMARY KEY, C_ID int(10) PRIMARY KEY, SK_NAME character(10), RANKS int(2), PROFICIENCY int(1))',[]).catch(e => console.log(e));
+        this.db.executeSql('CREATE TABLE IF NOT EXISTS GEAR(ID_NUM int(10) PRIMARY KEY, C_ID int(10) PRIMARY KEY, G_NAME character(32), G_WEIGHT int(4), AMOUNT int(4))', []).catch(e => console.log(e));
+        this.db.executeSql('CREATE TABLE IF NOT EXISTS WEAPONS(ID_NUM int(10) PRIMARY KEY, C_ID int(10) PRIMARY KEY, W_NAME character(32), DAMAGE character(6), TYPE character(12), RANGE int(4), CRITICAL character(6), W_WEIGHT int(4), W_AMOUNT int(4))', []).catch(e => console.log(e));
+        this.db.executeSql('CREATE TABLE IF NOT EXISTS ARMOR(ID_NUM int(10) PRIMARY KEY, C_ID int(10) PRIMARY KEY, AR_NAME character(32), AC_BONUS int(2), MAX_DEX int(2), CHECK int(2), SPELL_FAIL int(2), AR_WEIGHT int(4), AR_AMOUNT int(4))', []).catch(e => console.log(e));
+        this.db.executeSql('CREATE TABLE IF NOT EXISTS ABILITIES(ID_NUM int(10) PRIMARY KEY, C_ID int(10) PRIMARY KEY, AB_NAME character(32), AB_TYPE int(1), AB_LEVEL int(2))', []).catch(e => console.log(e));
+        this.db.executeSql('CREATE TABLE IF NOT EXISTS SPELLS(ID_NUM int(10) PRIMARY KEY, C_ID int(10) PRIMARY KEY, SP_NAME character(32), SP_LEVEL int(1))', []).catch(e => console.log(e));
         console.log("Tables probably made...")
     }
     
