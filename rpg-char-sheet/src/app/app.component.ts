4e71a20c25f89558c4da38c0cb3dabc8a79f1eec
theonepiecewondo <mdbaerlocher@lcmail.lcsc.edu>
2019-05-02 13:04:17 -0700

    Made a get all function for the stats table in the database

--- rpg-char-sheet/src/app/app.component.ts
+++ rpg-char-sheet/src/app/app.component.ts
@@ -15,12 +15,12 @@ export class AppComponent {
       title: 'Characters',
       url: '/characters',
       icon: 'body'
-    },
-    {
+    }
+    /*{
       title: 'Character',
       url: '/character',
       icon: 'body'
-    }
+    }*/
   ];
 
   constructor(
